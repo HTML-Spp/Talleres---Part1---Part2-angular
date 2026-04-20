540d402507ae1d92ff0408924e365f82609f9030
SamiP3-P <s.puentesp2@uniandes.edu.co>
2026-04-19 22:32:10 -0500

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,58 @@
+import { Serie } from "./serie.js";
+
+export const series: Serie[] = [
+  new Serie(
+    1,
+    "Breaking Bad",
+    "AMC",
+    5,
+    "Set in Albuquerque, the series tells the story of Walter White, a struggling high school chemistry teacher diagnosed with cancer.",
+    "https://www.amc.com/shows/breaking-bad",
+    "https://i0.wp.com/hipermediaciones.com/wp-content/uploads/2013/10/21225_breaking_bad.jpg?fit=1000%2C500&ssl=1"
+  ),
+  new Serie(
+    2,
+    "Orange is the New Black",
+    "Netflix",
+    6,
+    "A woman is sent to prison and adapts to life inside.",
+    "https://www.netflix.com/co/title/70242311",
+    "https://glup-glup.com/sites/default/files/oitnb.jpg"
+  ),
+  new Serie(
+    3,
+    "Game of Thrones",
+    "HBO",
+    7,
+    "Nine noble families fight for control of Westeros.",
+    "https://www.hbo.com/game-of-thrones",
+    "https://static.wikia.nocookie.net/hieloyfuego/images/2/2b/Game_of_Thrones_temporada_1_promocional.jpg/revision/latest?cb=20120630000249"
+  ),
+  new Serie(
+    4,
+    "The Big Bang Theory",
+    "CBS",
+    12,
+    "A group of scientists navigate life and relationships.",
+    "https://www.primevideo.com/-/es/detail/The-Big-Bang-Theory/0GOI318Q7ZJA5YTSWOTBZ09T9W",
+    "https://static.wikia.nocookie.net/warner-bros-entertainment/images/2/2b/The_Big_Bang_Theory_TV_Series.jpg/revision/latest?cb=20211207225643"
+  ),
+  new Serie(
+    5,
+    "Sherlock",
+    "BBC",
+    4,
+    "A modern update finds Sherlock Holmes solving crime.",
+    "https://www.netflix.com/co/title/70202589/",
+    "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_.jpg"
+  ),
+  new Serie(
+    6,
+    "A Very English Scandal",
+    "BBC",
+    2,
+    "The true story of a British politician scandal.",
+    "https://www.primevideo.com/-/es/detail/A-Very-English-Scandal/0HG4GRKBVPTFY6L6D8C896SN0M",
+    "https://m.media-amazon.com/images/M/MV5BNTQ5NDI0MTQ0MV5BMl5BanBnXkFtZTgwNDEzNTc1NTM@._V1_.jpg"
+  )
+];
---
+++ main.js
@@ -0,0 +1,43 @@
+import { series } from "./data.js";
+const tableBody = document.getElementById("series-table");
+const averageElement = document.getElementById("average");
+const detail = document.getElementById("serie-detail");
+
+series.forEach(serie => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${serie.id}</td>
+    <td><a href="${serie.link}" target="_blank">${serie.name}</a></td>
+    <td>${serie.channel}</td>
+    <td>${serie.seasons}</td>
+  `;
+    row.addEventListener("click", () => {
+        document.querySelectorAll("tr").forEach(r => r.classList.remove("table-active"));
+        row.classList.add("table-active");
+        mostrarDetalle(serie);
+    });
+    tableBody.appendChild(row);
+});
+function mostrarDetalle(serie) {
+    detail.innerHTML = `
+    <div class="card mt-2" style="width: 18rem;">
+      <img src="${serie.image}" 
+           class="card-img-top"
+           style="height: 200px; object-fit: cover;"
+           onerror="this.src='https://via.placeholder.com/300x200'">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.link}" target="_blank">${serie.link}</a>
+      </div>
+    </div>
+  `;
+}
+
+mostrarDetalle(series[0]);
+let total = 0;
+series.forEach(s => total += s.seasons);
+const promedio = total / series.length;
+averageElement.innerHTML = `
+  <strong>Seasons average: ${Math.round(promedio)}</strong>
+`;
